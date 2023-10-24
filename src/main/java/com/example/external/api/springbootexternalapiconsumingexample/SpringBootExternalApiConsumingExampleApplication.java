2e1421d0ef15db47b577d8ae036bd50d21fc8479
JunHokama <ejunhok@gmail.com>
2023-10-24 10:47:29 -0300

    initial commit

---
+++ src/main/java/com/example/external/api/springbootexternalapiconsumingexample/SpringBootExternalApiConsumingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.external.api.springbootexternalapiconsumingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExternalApiConsumingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExternalApiConsumingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/external/api/springbootexternalapiconsumingexample/SpringBootExternalApiConsumingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.external.api.springbootexternalapiconsumingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExternalApiConsumingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
